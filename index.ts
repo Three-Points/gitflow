17448211fe321c93c6ba8751a7eb702408acedca
roremDev <rorem.94.developer@gmail.com>
2022-01-22 21:27:38 -0600

    feat: ✨ added class A

--- index.ts
+++ index.ts
@@ -1,3 +1,12 @@
-// TODO - class A
+class A {
+  foo: string;
+  bar: string;
+  constructor(foo: string, bar: string) {
+    this.foo = foo;
+    this.bar = bar;
+  }
+}
+new A("foo", "bar");
+
 // TODO - class B
 // TODO - class C
